95984e62983744b0ca26ed6b1a887ad50a8e21ff
Gayane <you@example.com>
2018-03-12 12:13:47 +0400

    katarel em ashxatanq diveri het

--- client/game.js
+++ client/game.js
@@ -1,20 +1,6 @@
 
-function openCell(id){
-	moveRequest(id);
-	var image=document.getElementById(id);
-	if (elementValue=='G'){
-		image.src="gold.jpg";
-	}else if( elementValue=='B'){
-		image.src="bomb.jpg";
-	}else if(elementValue=='E'){
-		image.src="empty.jpg";
-	}
-	var gameStatusElement= document.getElementById('gameStatus');
-	gameStatusElement.innerHTML=gameStatus;
-	var lifeNumberElement= document.getElementById('lifeNumber');
-	lifeNumberElement.innerHTML=lifeNumber;
-	var goldNumberElement= document.getElementById('goldNumber');
-	goldNumberElement.innerHTML=goldNumber;
+function openCell(id, y, x){
+	moveRequest(y, x, id);
 }
 
 function changeimage(id,source){
@@ -22,34 +8,50 @@ function changeimage(id,source){
   image.src=source;
 
 }
-var coordinateX =0; 
-var coordinateY =1;
-var elementValue='E';
-var gameStatus='In progress';
-var lifeNumber=5;
-var goldNumber=2;
-function moveRequest(id){
-  /*axios.get('localhost:8080/goldminerthegame/move?x=' + x + '&y=' + y)
+var coordinateX=0; 
+var coordinateY=1;
+var elementValue='';
+var gameStatus='';
+var lifeNumber=3;
+var goldNumber=0;
+function moveRequest(y,x,id){
+  axios.get('http://localhost:8080/goldminerthegame/move?x=' + x + '&y=' + y)
     .then(function(response){
-      console.log('Response gold: ' + response.gold); // ex.: { user: 'Your User'}
-      console.log('Response status: ' +response.status); // ex.: 200
-      console.log('Response info: ' +response.info);
-      console.log('Response value: ' +response.value);
-      console.log('Response lives: ' +response.lives);
-      for(property in response){
-        //alert(property);
+      if(response.hasOwnProperty('data')){
+        /*for(property in response.data){
+          console.log(property)
+        }*/
+        if(response.data.hasOwnProperty('value')){
+          elementValue = response.data.value;
+        }
+        if(response.data.hasOwnProperty('gold')){
+          goldNumber = response.data.gold;
+        }
+        if(response.data.hasOwnProperty('lives')){
+          lifeNumber = response.data.lives;
+        }
+        if(response.data.hasOwnProperty('status')){
+          gameStatus = response.data.status;
+        }
       }
-      if(response.data.hasOwnProperty('id')){
-        alert('d'+response.data.id);
-      }
-      return response;
+      handleResponse(id,y,x);
+      //return response;
     }); 
-    var response=moveRequest(1,1);
-	if (response.value=='G'){
-		changeimage(id,"gold.jpg")
-	}
-	else if(){
-		
-	}
-    */
+}
+
+function handleResponse(id, y, x){
+  var image=document.getElementById(id);
+  if (elementValue=='G'){
+    image.src="gold.jpg";
+  }else if( elementValue=='B'){
+    image.src="bomb.jpg";
+  }else if(elementValue=='E'){
+    image.src="empty.jpg";
+  }
+  var gameStatusElement= document.getElementById('gameStatus');
+  gameStatusElement.innerHTML=gameStatus;
+  var lifeNumberElement= document.getElementById('lifeNumber');
+  lifeNumberElement.innerHTML=lifeNumber;
+  var goldNumberElement= document.getElementById('goldNumber');
+  goldNumberElement.innerHTML=goldNumber;
 }
